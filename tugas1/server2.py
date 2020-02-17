2c6c148dd7a3e5bbbb451ec4441f4a9a736996fb
jihannabiila <jihannabila26@gmail.com>
2020-02-18 00:58:15 +0700

    3 port

---
+++ tugas1/server2.py
@@ -0,0 +1,27 @@
+import sys
+import socket
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Bind the socket to the port
+server_address = ('localhost', 31001)
+print('starting up on %s port %s' % server_address)
+sock.bind(server_address)
+# Listen for incoming connections
+sock.listen(1)
+while True:
+	# Wait for a connection
+	print('waiting for a connection')
+	connection, client_address = sock.accept()
+	print('connection from', client_address)
+	# Receive the data in small chunks and retransmit it
+	while True:
+		data = connection.recv(32)
+		print('received "%s"' % data.decode())
+		if data:
+			print('sending data back to the client')
+			connection.sendall(data)
+		else:
+			print('no more data from', client_address)
+			break
+	# Clean up the connection
+	connection.close()
